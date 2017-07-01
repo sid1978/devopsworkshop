bfcc773d0f168470ef37422bc6318ee87a57fb2f
Siddharth <sid_sj@yahoo.com>
2017-07-01 14:14:11 +0530

    Created HelloWorld Class

---
+++ DevOpsLeap/src/com/workshop/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.workshop;
+
+/**
+ * @author admin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
